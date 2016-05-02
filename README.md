b4b7f349efcc11b8a641ca84467f49669ddb6c64
Dwaraka Nath <dtsdwarak@hotmail.com>
2016-05-02 23:50:26 +0530

    Add dotfile configs

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# dotfiles
-Shell configs
+# Dotfiles
+
+## What?
+* Small shell enhancements to make your terminal attractive!
+* Works with ```zsh, bash```.
+
+## Install
+```
+$ chmod +x install.sh && ./install.sh # Run as sudo
+```
+
+## Catches
+Dependency installation handled only for Debian and its downstream distros. Install missing packages if you run a different OS.
+
+## License
+[GNU GPL v3](http://choosealicense.com/licenses/gpl-3.0/)
+
+----
+
+Show your shell some :heart:
+
---
+++ install.sh
@@ -0,0 +1,31 @@
+# Install dependencies
+apt-get -y install python-pip i3lock vim
+pip install Pygments
+
+#Copy files
+cp -r dwarak_dotfiles $HOME
+
+# Install configs over Zsh
+if [[ -z $(grep "dwarak_dotfiles" $HOME/.zshrc) ]];
+then
+    echo 'source $HOME/dwarak_dotfiles/dwarak_zshrc' >> $HOME/.zshrc
+fi
+
+# Install configs over Bash
+if [[ -z $(grep "dwarak_dotfiles" $HOME/.bashrc) ]];
+then
+    echo 'source $HOME/dwarak_dotfiles/dwarak_bashrc' >> $HOME/.bashrc
+fi
+
+if [[ -z $(grep "bashrc" $HOME/.bash_profile) ]];
+then
+    echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+fi
+
+# Pull vim configs over
+if [[ -z $(grep "vim.rc" $HOME/.vimrc) ]];
+then
+    echo 'source $HOME/dwarak_dotfiles/vim/vim.rc' >> $HOME/.vimrc
+fi
+
+echo "Install successful. Open a new terminal to see changes. :-) "
