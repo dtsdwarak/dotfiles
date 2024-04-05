1e27193764d700b1441410e70fd57c103aba6cff
Dwaraka Nath <dtsdwarak@hotmail.com>
2024-04-05 23:30:40 +0530

    Fix Ubuntu installs

--- install.sh
+++ install.sh
@@ -12,29 +12,25 @@ printf "\n${GREEN}Installing generic dependencies for Ubuntu...${RESET_COLOR}\n\
 
 # Install dependencies
 sudo apt update && sudo apt upgrade
-sudo apt -y install pydf build-essential libssl-dev postgresql-client pv jq fonts-inconsolata python3-pip i3lock vim htop lighttpd xsel pigz ncdu tmux rbenv ruby-build direnv thefuck software-properties-common stow bash zsh coreutils img2pdf
-# python3 -m ensurepip
-sudo pip install Pygments tldr csvkit pgcli pyyaml
+sudo apt -y install pydf build-essential libyaml-dev libssl-dev postgresql-client \
+pv jq fonts-inconsolata python3-pip i3lock vim htop lighttpd xsel pigz ncdu tmux \
+ruby-build direnv thefuck software-properties-common stow bash zsh coreutils img2pdf
 
 # fzf install
 # Install via git to include shell-bindings since it is currently not supported if installed via package manager in Ubuntu
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install || echo "fzf already installed" 
 
-# TODO
-# Automate installation of asdf plugins/versions for python, ruby, rust and node
-asdf plugin add ruby
-# For ruby version install check https://stackoverflow.com/a/77857095/2981954
-asdf plugin add nodejs
-asdf plugin add python
-asdf plugin add rust
-cargo install bat exa fd-find procs du-dust ripgrep eva lsd
-asdf reshim rust # Need to reshim post cargo binary installs https://github.com/code-lever/asdf-rust/issues/14
+# ASDF Install
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0 || echo "asdf already installed"
 
 # Install SDKMan
+if [ ! -d "$HOME/.sdkman" ]; then
+    echo "sdkman could not be found. Installing..."
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
 
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+printf "\n\n${GREEN} Copying source files... ${RESET_COLOR} \n"
 
 #Copy files
 cp -r dwarak_dotfiles $HOME
@@ -80,6 +76,8 @@ fi
 # Setup Git
 ##################
 
+printf "\n\n${GREEN} Setting up Git... ${RESET_COLOR} \n"
+
 # Install gitconfig
 cat $HOME/dwarak_dotfiles/git/gitconfig > $HOME/.gitconfig
 
@@ -90,6 +88,8 @@ mkdir -p $HOME/.zsh/functions && cp $HOME/dwarak_dotfiles/git/git-completion.zsh
 ##################
 # Setup VIM
 ##################
+
+printf "\n\n${GREEN} Setting up vim configs... ${RESET_COLOR} \n"
 
 # Pull vim configs over
 touch $HOME/.vimrc
@@ -106,6 +106,8 @@ cp -R $HOME/dwarak_dotfiles/vim/colors/* $HOME/.vim/colors/
 
 ##################
 
+printf "\n\n${GREEN} Setting up kitty... ${RESET_COLOR} \n"
+
 # Setup Kitty
 mkdir -p $HOME/.config/kitty/
 cp $HOME/dwarak_dotfiles/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
@@ -120,7 +122,44 @@ mkdir -p $HOME/apps/
 
 ##############################
 
+case `echo $SHELL | rev | cut -d'/' -f1 | rev` in
+  bash)
+    source $HOME/.bashrc
+    ;;
+  zsh)
+    source $HOME/.zshrc
+    ;;
+esac
 
-source $HOME/.bashrc
-source $HOME/.zshrc
+printf "\n\n${GREEN} Copied all source files ${RESET_COLOR} \n"
+
+# For ruby version install check https://stackoverflow.com/a/77857095/2981954
+printf "\n\n${GREEN} Installing ruby... ${RESET_COLOR} \n"
+asdf plugin add ruby || echo "Ruby already installed"
+asdf install ruby 3.3.0 || echo "Ruby already installed"
+asdf global ruby 3.3.0 || echo "Ruby already installed"
+
+printf "\n\n${GREEN} Installing nodejs... ${RESET_COLOR} \n"
+asdf plugin add nodejs || echo "nodejs already installed"
+asdf install nodejs 20.12.0 || echo "nodejs already installed"
+asdf global nodejs 20.12.0 || echo "nodejs already installed"
+
+printf "\n\n${GREEN} Installing python... ${RESET_COLOR} \n"
+asdf plugin add python || echo "python already installed"
+asdf install python 3.12.0 || echo "python already installed"
+asdf global python 3.12.0 || echo "python already installed"
+
+printf "\n\n${GREEN} Installing pip packages... ${RESET_COLOR} \n"
+sudo pip install Pygments tldr csvkit pgcli pyyaml || echo "All packages already installed"
+
+printf "\n\n${GREEN} Installing rust... ${RESET_COLOR} \n"
+asdf plugin add rust || echo "rust already installed"
+asdf install rust 1.77.0 || echo "rust already installed"
+asdf global rust 1.77.0 || echo "rust already installed"
+
+printf "\n\n${GREEN} Installing rust binaries... ${RESET_COLOR} \n"
+cargo install bat exa fd-find procs du-dust ripgrep eva lsd
+asdf reshim rust # Need to reshim post cargo binary installs https://github.com/code-lever/asdf-rust/issues/14
+
+
 printf "\n\n${GREEN}Dotfile installation successful! ${RESET_COLOR} \n"
